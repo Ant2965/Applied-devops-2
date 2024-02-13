bdc079eeb12da7e19c5c73c335613d67d229f07a
Ant2965 <antrikshbahri@gmail.com>
2024-02-13 21:39:46 +0530

    Added

--- Readme.md
+++ Readme.md
@@ -1,6 +1,10 @@
-# Calculator-App-in-Spring-Boot
+# Spring Boot Hello World Example Project.
 
-This is a calculator web application developed using Spring Boot..
 
-This application has functions like Add, Subtract, Multiply, Divide and exponent. 
-
+- check is user a root user.
+- apt-get update
+- apt-get install maven
+- mvn test
+- mvn package
+- apt-get install tomcat9
+- cp -rvf target/hello-world-0.0.1-SNAPSHOT.war /var/lib/tomcat9/webapps/app.war
---
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -0,0 +1,34 @@
+package com.springhow.example.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class HelloWorldApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HelloWorldApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class);
+    }
+
+
+    @RequestMapping("/")
+    String helloWorld() {
+        return "Hello this website is deployed on TOMCAT    Antriksh\r\n" + //
+                        "        Avni\r\n" + //
+                        "        Abhay\r\n" + //
+                        "        Akshay\r\n" + //
+                        "        Gaurav\r\n" + //
+                        "        Diya ";
+    }
+
+}
